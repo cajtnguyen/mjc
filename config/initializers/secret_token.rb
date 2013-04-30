9361daca6a3cb6ff43f8cbcc97e80848e9a89d68
John Nguyen <cajtnguyen@gmail.com>
2013-04-29 20:11:17 -0700

    Initial Commit MJC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mjc::Application.config.secret_token = '3f805a5605e89639ced43afcd987529dc773bfa084d5d1c6ec587d0db22182d146ff1aa102f5bcce677a7a8a54003980b48990a783c1b2e60706a7c52b765fb7'
